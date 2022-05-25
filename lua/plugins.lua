849eef1c595d2c5a34bbf17b437a52c2d23b2578
yoshiro <saipinpin7@gmail.com>
2022-05-25 15:09:36 +0630

    done

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,4 +3,24 @@ return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   use 'navarasu/onedark.nvim'
 
+  -- LSP
+  use {
+    "williamboman/nvim-lsp-installer",
+    {
+      "neovim/nvim-lspconfig",
+      config = function()
+        require("nvim-lsp-installer").setup {}
+        local lspconfig = require("lspconfig")
+        lspconfig.sumneko_lua.setup {}
+      end
+    }
+  }
+  -- Autocomplete
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/vim-vsnip'
 end)
